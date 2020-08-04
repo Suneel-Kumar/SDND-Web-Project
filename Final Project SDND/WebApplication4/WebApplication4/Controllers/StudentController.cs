031bb8330bd11f0b7d68c7ccc6c742ecd84cdad2
Suneel Kumar <malhisuneel@gmail.com>
2020-08-05 01:14:11 +0500

    SDND_SPA_Module_Project

---
+++ Final Project SDND/WebApplication4/WebApplication4/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult elearn()
+        {
+            return View();
+        }
+
+
+        public ActionResult slide()
+        {
+            return View();
+        }
+
+
+        public ActionResult quiz()
+        {
+
+            return View();
+
+        }
+    }
+}
